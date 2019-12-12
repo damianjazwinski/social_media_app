abfa0a81b3a8483e7f1ac945a9e432a8fb067489
Damian Jaźwiński <damian.jazwinski@nask.pl>
2019-12-12 16:14:31 +0100

    First endpoint added

--- sma-functions/functions/index.js
+++ sma-functions/functions/index.js
@@ -1,9 +1,49 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+var serviceAccount = require("../social-app-b9983-firebase-adminsdk-r926o-930645935c.json");
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello world!");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://social-app-b9983.firebaseio.com"
 });
+
+const express = require("express");
+const app = express();
+
+app.get("/screams", (request, response) => {
+  admin
+    .firestore()
+    .collection("screams")
+    .get()
+    .then(data => {
+      let screams = [];
+      data.forEach(doc => {
+        screams.push(doc.data());
+      });
+      return response.json(screams);
+    })
+    .catch(err => console.error(err));
+});
+
+app.post("/scream", (request, response) => {
+  const newScream = {
+    body: request.body.body,
+    userHandle: request.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  };
+  admin
+    .firestore()
+    .collection("screams")
+    .add(newScream)
+    .then(doc => {
+      return response.json({
+        message: `document ${doc.id} created successfully`
+      });
+    })
+    .catch(err => {
+      response.status(500).json({ error: "failed to create scream" });
+    });
+});
+
+exports.api = functions.https.onRequest(app);
